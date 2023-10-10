0716dbcf5a50eb9d093584a40846096b98434cf4
Farhan Zaman <farhanzaman1895@gmail.com>
2023-10-10 15:13:58 -0600

    Lab 3

--- Lab 3/lab3V1.py
+++ Lab 3/lab3V1.py
@@ -22,12 +22,57 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-print('Version 1')
-# ------------Students edit/write their code below here--------------------------
-# ------------Remove any code that is unnecessary--------------------------
-counter = []
-for i in range(0, 216):
-    counter += [i]
-print(f'counter(last value) = {counter[-1]} \n')
-plt.plot(range(0, 216), counter)
+## CONSTANTS
+NUMBER_OF_MONTHS = 12 * 18
+MONTHLY_CONTRIBUTION = 200
+START_AMOUNT = 2000
+MONTHLY_INTEREST = 0.0625
+TUITION_INTEREST = 0.07
+
+# Arrays for tuition interest
+artTuit = [5550]
+sciTuit = [6150]
+engTuit = [6550]
+
+# Initialize y axis for graph
+monthlyAmount = [START_AMOUNT]
+
+## CALCULATION
+for i in range(1, NUMBER_OF_MONTHS):
+    monthlyAmount += [monthlyAmount[i - 1] + (monthlyAmount[i - 1] * (MONTHLY_INTEREST / 12)) + MONTHLY_CONTRIBUTION]
+
+for i in range(1, 22):
+    artTuit += [ artTuit[i-1] * (1 + TUITION_INTEREST) ]
+    sciTuit += [ sciTuit[i-1] * (1 + TUITION_INTEREST) ]
+    engTuit += [ engTuit[i-1] * (1 + TUITION_INTEREST) ]
+
+totalArtTuit = 0
+totalSciTuit = 0
+totalEngTuit = 0
+
+for i in range(0, 4):
+    totalArtTuit += artTuit[-(i + 1)]
+    totalSciTuit += sciTuit[-(i + 1)]
+    totalEngTuit += engTuit[-(i + 1)]
+
+## OUTPUTS
+print("Version 1 - Solution")
+print(f"The savings ammount is {format(monthlyAmount[-1], '.2f')}")
+print(f"The cost of the arts program is ${format(totalArtTuit, '.2f')}")
+print(f"The cost of the science program is ${format(totalSciTuit, '.2f')}")
+print(f"The cost of the engineering program is ${format(totalEngTuit, '.2f')}")
+
+# Make the labels for the ticks along x axis
+xTickLabels = np.char.mod("%d", np.arange(0, 19))
+
+# Plot Graph
+fig, ax = plt.subplots()
+ax.plot(range(0, NUMBER_OF_MONTHS), monthlyAmount, label = "Saving Balance")
+ax.hlines(totalArtTuit, 0, NUMBER_OF_MONTHS, color = "orange", label = "Arts")
+ax.hlines(totalSciTuit, 0, NUMBER_OF_MONTHS, color = "green", label = "Science")
+ax.hlines(totalEngTuit, 0, NUMBER_OF_MONTHS, color = "red", label = "Engineering")
+ax.set(xticks = range(0, NUMBER_OF_MONTHS + 1, 12), xticklabels = xTickLabels)
+ax.axis([0, NUMBER_OF_MONTHS + 1, 0, 100000])
+ax.legend()
+ax.margins(x=0, y=0)
 plt.show()
