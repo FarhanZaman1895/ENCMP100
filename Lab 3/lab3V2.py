411279d52641f55aae69a8cd8ef32a287f97116e
Farhan Zaman <farhanzaman1895@gmail.com>
2023-10-12 11:33:12 -0600

    Lab 3 version 2 uncommented

--- Lab 3/lab3V2.py
+++ Lab 3/lab3V2.py
@@ -27,6 +27,7 @@ import numpy as np
 ## CONSTANTS
 NUMBER_OF_MONTHS = 12 * 18
 START_AMOUNT = 2000
+MONTHLY_CONTRIBUTION = 200
 MONTHLY_INTEREST = 0.0625
 TUITION_INTEREST = 0.07
 
@@ -36,7 +37,6 @@ sciTuit = [6150]
 engTuit = [6550]
 
 # Variable Monthly Contribution
-monthlyContribution = 200
 
 # Initialize y axis for graph
 monthlyAmount = [START_AMOUNT]
@@ -45,7 +45,7 @@ monthlyAmount = [START_AMOUNT]
 
 # Saving Calculation
 for i in range(1, NUMBER_OF_MONTHS):
-    monthlyAmount += [monthlyAmount[i - 1] + (monthlyAmount[i - 1] * (MONTHLY_INTEREST / 12)) + monthlyContribution]
+    monthlyAmount += [monthlyAmount[i - 1] + (monthlyAmount[i - 1] * (MONTHLY_INTEREST / 12)) + MONTHLY_CONTRIBUTION]
 
 # Tuition Calculation
 for i in range(1, 22):
@@ -62,13 +62,54 @@ for i in range(0, 4):
     totalSciTuit += sciTuit[-(i + 1)]
     totalEngTuit += engTuit[-(i + 1)]
 
-## OUTPUTS
+## SAVINGS/TUITION CALCULATION
 print("Version 1 - Solution")
 print(f"The savings ammount is {format(monthlyAmount[-1], '.2f')}")
 print(f"The cost of the arts program is ${format(totalArtTuit, '.2f')}")
 print(f"The cost of the science program is ${format(totalSciTuit, '.2f')}")
 print(f"The cost of the engineering program is ${format(totalEngTuit, '.2f')}")
+print("")
 
+## SAVINGS OPTIMIZATION
+print("Version 2 - Solution")
+desiredProgram = input("Enter a program 1.Arts, 2.Science, 3. Engineering :")
+
+condition = True
+while condition:
+    try:
+        desiredProgram = int(desiredProgram)
+    except:
+        print("Please enter a number from 1 to 3: ")
+    else:
+        if desiredProgram in range(1, 4):
+            condition = False
+
+condition = True
+incrementCount = 1
+
+if desiredProgram == 1:
+    requiredAmmount = totalArtTuit
+elif desiredProgram == 2:
+    requiredAmmount = totalSciTuit
+else:
+    requiredAmmount = totalEngTuit
+
+while condition:
+    incrementCount += 1
+    optimizeArr = [2000]
+    for i in range(1, NUMBER_OF_MONTHS):
+        optimizeArr += [optimizeArr[i - 1] + (optimizeArr[i - 1] * (MONTHLY_INTEREST / 12)) + incrementCount]
+    if optimizeArr[-1] >= requiredAmmount:
+        condition = False
+
+if desiredProgram == 1:
+    print("Congratulation!!! You have saved enough for the arts program")
+elif desiredProgram == 2:
+    print("Unfortunately!!! You do not have enough saved for the science program")
+else:
+    print("Unfortunately!!! You do not have enough saved for the engineering program")
+
+print(f"The optimal monthly contribution amount is ${incrementCount}")
 # Make the labels for the ticks along x axis
 xTickLabels = np.char.mod("%d", np.arange(0, 19))
 
